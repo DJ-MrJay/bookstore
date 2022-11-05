76c271d172e9c049b947d41881178d13d730e73f
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-11-05 15:45:23 +0300

    Fix linters errors

---
+++ src/pages/Books.js
@@ -0,0 +1,13 @@
+import BookList from '../components/BookList';
+import Form from '../components/Form';
+
+function Home() {
+  return (
+    <>
+      <BookList />
+      <Form />
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/Categories.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkingStatus } from '../redux/categories/categoriesSlice';
+
+function Categories() {
+  const status = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+
+  const antIcon = (
+    <LoadingOutlined
+      style={{
+        fontSize: 24,
+      }}
+      spin
+    />
+  );
+
+  return (
+    <>
+      <div className="container">
+        <Spin indicator={antIcon} />
+        <button
+          type="button"
+          className="statusBtn"
+          onClick={() => dispatch(checkingStatus())}
+        >
+          Check status
+        </button>
+        <span className="statusMsg">{status.message}</span>
+      </div>
+    </>
+  );
+}
+
+export default Categories;
